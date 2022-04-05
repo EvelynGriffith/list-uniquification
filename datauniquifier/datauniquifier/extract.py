8b0e102c4d1f83ab201e83f30f6f87304fc158b0
EvelynGriffith <griffith01@allegheny.edu>
2022-04-05 15:41:33 -0400

    trying to add to extract

--- datauniquifier/datauniquifier/extract.py
+++ datauniquifier/datauniquifier/extract.py
@@ -22,12 +22,16 @@ import csv
 def extract_data_given_column(data: str, column: int) -> List[str]:
     """Extract a specified data column from the provided textual contents."""
     # create an empty list of the data
-    data = []
+    data_collection = []
     # note that the data file:
     # --> contains two columns
     # --> each of which contains textual data
     # --> refer to the file called input/data.txt to learn more about
     # the format of the comma separated value (CSV) file that we must parse
-    # --> TODO: iterate through each line of the file and extract the current job
-    # TODO: ---> extract the specified column that contains the requested data
-    # TODO: return the list of all of the specified column
+    # --> iterate through each line of the file and extract the current job
+    for item in data:
+        data = file.open("input/data.txt", "r")
+        column = data.splitlines(",")
+        item.append()
+    # ---> extract the specified column that contains the requested data
+    # return the list of all of the specified column
--- datauniquifier/datauniquifier/uniquify.py
+++ datauniquifier/datauniquifier/uniquify.py
@@ -21,30 +21,36 @@ def timing(function):
     return wrap
 
 
-# TODO: Read the reference for a description of the functions:
+# Read the reference for a description of the functions:
 #
 # https://www.peterbe.com/plog/fastest-way-to-uniquify-a-list-in-python-3.6
 #
 # Note that this module does not contain all of the functions in the post
 
-# TODO: If you would like to do so, add and then experimentally evaluate more methods
+# If you would like to do so, add and then experimentally evaluate more methods
 
-# TODO: Make sure that you understand the purpose of the timing annotation for these functions
+# Make sure that you understand the purpose of the timing annotation for these functions
 # TODO: Add the timing annotation to each of the following functions
 
 def unique_set(data: List[str]) -> List[str]:
     """Perform uniquification of the input list of strings and return results in a list of strings."""
-    # TODO: Add the source code for method f7
-    # TODO: You may need to add type hints or statements to support mypy checking
+    # Add the source code for method f7
+    # You may need to add type hints or statements to support mypy checking
+    # Not order preserving
+    return list(set(data))
 
 
 def unique_setcomprehension(data: List[str]) -> List[str]:
     """Perform uniquification of the input list of strings and return results in a list of strings."""
-    # TODO: Add the source code for method f8
-    # TODO: You may need to add type hints or statements to support mypy checking
+    # Add the source code for method f8
+    # You may need to add type hints or statements to support mypy checking
+    # Order preserving
+    seen = set()
+    return [x for x in data if x not in seen and not seen.add(x)]
 
 
 def unique_dictionary(data: List[str]) -> List[str]:
     """Perform uniquification of the input list of strings and return results in a list of strings."""
-    # TODO: Add the source code for method f12
-    # TODO: You may need to add type hints or statements to support mypy checking
+    # Add the source code for method f12
+    # You may need to add type hints or statements to support mypy checking
+    return list(dict.fromkeys(data))
